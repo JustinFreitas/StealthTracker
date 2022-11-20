35a1f310b9e4e09d36aa27099c56e53f59eaeb54
JustinFreitas <justin.s.freitas@me.com>
2022-11-19 17:04:55 -0700

    build script fix

--- build/build-stealthtracker-zip.bat
+++ build/build-stealthtracker-zip.bat
@@ -5,7 +5,7 @@ copy ..\readme.txt out\StealthTracker\
 mkdir out\StealthTracker\scripts
 copy ..\scripts\stealthtracker.lua out\StealthTracker\scripts\
 mkdir out\StealthTracker\graphics\icons
-copy ..\graphics\icons\stealth_icon.png out\StealthTracker\graphics\
+copy ..\graphics\icons\stealth_icon.png out\StealthTracker\graphics\icons
 cd out
 CALL ..\zip-items StealthTracker
 rmdir /S /Q StealthTracker\
